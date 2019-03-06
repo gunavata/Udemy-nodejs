81ac9115d2351e2d39989f603be63ae569fa7ab8
gunavata <gunavata@hotmail.com>
2019-03-06 16:13:28 +0800

    Using ExpressJS
    Started using Express JS

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes.js');
+const app = express();
 
+app.use('/', (req, res, next) => {
+    console.log("Always runs!");
+    next();
+})
 
-const server = http.createServer(routes);
+app.use('/add-product', (req, res, next) => {
+    console.log('Product page!');
+    res.send("<form action='/product' method='post'><input type='text' name ='title'><button type='submit'>Add product!</button></form>");
+});
 
-server.listen(3000);
+app.use('/product', (req, res, next) => {
+    console.log('Added product page!');
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('Main page!');
+    res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);

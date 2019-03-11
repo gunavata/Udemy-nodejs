6ce9e934e6182abbe4f3999f0924a023f3719996
gunavata <gunavata@hotmail.com>
2019-03-11 19:39:00 +0800

    Fixed error with cart
    1a) let cart is no longer used as cart itself is not used (updatedCart is used instead)
    1b) JSON writefile was writing cart instead of updatedCart which held no value (hence the fuckin purge)
    
    2) product = updatedcart find instead of findIndex (find index only returns back the first true argument of that index)

--- models/cart.js
+++ models/cart.js
@@ -38,18 +38,17 @@ module.exports = class Cart {
     };
     static deleteProduct(id, productPrice) {
         fs.readFile(p, (err, fileContent) => {
-            let cart = { products: [], totalPrice: 0 };
             if (err) {
                 return;
             }
             const updatedCart = { ...JSON.parse(fileContent) };
-            const product = updatedCart.products.findIndex(prod => prod.id === id);
+            const product = updatedCart.products.find(prod => prod.id === id);
             if (!product) {
                 return;
             }
             updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
             updatedCart.totalPrice = updatedCart.totalPrice - productPrice * product.qty;
-            fs.writeFile(p, JSON.stringify(cart), err => {
+            fs.writeFile(p, JSON.stringify(updatedCart), err => {
                 console.log(err);
             });
         });

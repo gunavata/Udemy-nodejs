6b76caa8e6fd9021cb9239f58a5ef67c451c162a
gunavata <gunavata@hotmail.com>
2019-03-13 20:20:34 +0800

    130+ started using MYSQL

--- models/product.js
+++ models/product.js
@@ -12,11 +12,13 @@ module.exports = class Product {
     }
 
     save() {
-       
+        return db.execute(
+            'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+            [this.title, this.price, this.imageUrl, this.description])
     };
 
     static deleteById(id) {
-        
+
     }
 
     static fetchAll() {
@@ -24,6 +26,6 @@ module.exports = class Product {
     }
 
     static findById(id) {
-        
+        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
     }
 }

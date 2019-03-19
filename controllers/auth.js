65141e6dd4925dd842010c6f691991b770a15b0c
gunavata <gunavata@hotmail.com>
2019-03-19 23:04:38 +0800

    265 Sending emails

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,20 +4,28 @@ const bcrypt = require('bcryptjs')
 
 exports.getLogin = (req, res, next) => {
     console.log('Log in page!')
+    let message = req.flash('error')
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render("auth/login", {
         pageTitle: "Login Page!",
         path: "/login",
-        errorMessage: req.flash('error')
+        errorMessage: message
     })
 }
 
 exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    User.findOne({ email: email })
+    User.findOne({
+            email: email
+        })
         .then(user => {
             if (!user) {
-                req.flash('error', 'Invalid email or password!');
+                req.flash('error', 'Invalid Email or Password')
                 return res.redirect('/login');
             } else {
                 bcrypt.compare(password, user.password)
@@ -30,6 +38,7 @@ exports.postLogin = (req, res, next) => {
                                 return res.redirect('/');
                             })
                         } else {
+                            req.flash('error', 'Invalid Password');
                             res.redirect('/login');
                         }
                     })
@@ -55,9 +64,12 @@ exports.postSignup = (req, res, next) => {
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
     User
-        .findOne({ email: email })
+        .findOne({
+            email: email
+        })
         .then(userDoc => {
             if (userDoc) {
+                req.flash('error', 'Email already exists')
                 return res.redirect('/signup')
             } else {
                 return bcrypt
@@ -66,7 +78,9 @@ exports.postSignup = (req, res, next) => {
                         const user = new User({
                             email: email,
                             password: hashedPassword,
-                            cart: { items: [] }
+                            cart: {
+                                items: []
+                            }
                         })
                         return user.save();
                     })
@@ -81,8 +95,15 @@ exports.postSignup = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+    let message = req.flash('error')
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
+        errorMessage: message
     });
 };
